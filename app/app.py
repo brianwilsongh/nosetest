0b4abada02445b7773dbf39cf6018e60864314f4
wilsontheory <wilson.brian90@gmail.com>
2020-01-21 03:16:37 -0500

    Make sure file to be uploaded is removed

--- app/app.py
+++ app/app.py
@@ -4,6 +4,7 @@ import requests
 import utils
 import collections
 import csv
+import os 
 
 BASE_URL = "http://swapi.co/api/"
 PASTEBIN = "http://httpbin.org/post"
@@ -16,12 +17,15 @@ def main():
     people.sort(key=lambda x: len(x['films']), reverse=True)
     people = people[:9]
     people.sort(key=lambda x: int(x['height']), reverse=True)
-    with open(DEFAULT_FILENAME, "w", newline="") as csvfile:
-        writer = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
-        writer.writerow(["name", "species", "height", "appearances"])
-        for char in people:
-            writer.writerow([char['name'], utils.get_one(char['species'][0])['name'], char['height'], len(char['films'])])
-    upload_status = utils.send_file_to_url(PASTEBIN, DEFAULT_FILENAME)
+    try:
+        with open(DEFAULT_FILENAME, "w", newline="") as csvfile:
+            writer = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
+            writer.writerow(["name", "species", "height", "appearances"])
+            for char in people:
+                writer.writerow([char['name'], utils.get_one(char['species'][0])['name'], char['height'], len(char['films'])])
+        upload_status = utils.send_file_to_url(PASTEBIN, DEFAULT_FILENAME)
+    finally:
+        os.remove(DEFAULT_FILENAME)
     if upload_status:
         print("File uploaded successfully!")
     else:
