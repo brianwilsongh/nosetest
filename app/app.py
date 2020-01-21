d01e11d9755a329ef32b0e171162bdf7ee68695e
wilsontheory <wilson.brian90@gmail.com>
2020-01-21 03:11:05 -0500

    Remove unnecessary code

--- app/app.py
+++ app/app.py
@@ -5,14 +5,6 @@ import utils
 import collections
 import csv
 
-# Using the Star Wars API (https://swapi.co/)
-# 
-# TODO: 1. Find the ten characters who appear in the most Star Wars films
-# TODO: 2. Sort those ten characters by height in descending order (i.e., tallest first)
-# TODO: 3. Produce a CSV with the following columns: name, species, height, appearances
-# TODO: 4. Send the CSV to httpbin.org
-# TODO: 5. Create automated tests that validate your code
-
 BASE_URL = "http://swapi.co/api/"
 PASTEBIN = "http://httpbin.org/post"
 DEFAULT_FILENAME = "file.csv"
@@ -20,8 +12,6 @@ DEFAULT_FILENAME = "file.csv"
 def main():
     logging.basicConfig(format='%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
     datefmt='%Y-%m-%d:%H:%M:%S', level=logging.DEBUG)
-    # logging.getLogger().setLevel(logging.DEBUG)
-    # logging.Formatter('')
     people = utils.get_all(BASE_URL+"people")
     people.sort(key=lambda x: len(x['films']), reverse=True)
     people = people[:9]
